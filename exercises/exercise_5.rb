8232cfaa99589c481eb2522aafb44920b94e9e55
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-12-11 12:13:33 -0800

    use calculation methods

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Store.calculate(:sum, :annual_revenue)
+puts Store.calculate(:average, :annual_revenue)
+puts Store.where('annual_revenue > ?', 1000000).calculate(:count, :all)
+
+
